9223a8fcc7d63ec303fe092541450433fcb67543
marlonc98 <mackiller1998@gmail.com>
2020-10-10 18:35:21 -0500

    routing beneficiarie

--- diastole/src/App.js
+++ diastole/src/App.js
@@ -1,18 +1,25 @@
 import React from "react";
 import './App.css';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import Layout from './components/Layout';
 import Router from './router';
 import { store } from './store';
+import ListBeneficiaries from "./pages/ListBeneficiaries";
+import DetailedBeneficiarie from "./pages/DetailedBeneficiare";
+import RequestCodeBeneficiarie from "./pages/RequestCodeBeneficiarie";
 
 const App = () => {
   return(
     <Provider store={store}>
       <BrowserRouter>
         <Layout>
-          <Router />
+          <Router>
+            <Route path="lista-beneficiarios" component={ListBeneficiaries}/>
+            <Route path="detalles-beneficiarios/:idBeneficiario" component={DetailedBeneficiarie}/>
+            <Route path="verificar-beneficiario/:idBeneficiario" component={RequestCodeBeneficiarie}/>
+          </Router>
         </Layout>
       </BrowserRouter>
     </Provider>
