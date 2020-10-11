3aada6bafefd55dd8e6e45a114e7d78c2501973c
Andrés Morales <mora200217@hotmail.com>
2020-10-11 14:45:58 -0500

    [Update] Background Login

--- diastole/src/components/Login.js
+++ diastole/src/components/Login.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import "./../App.css";
-
+import BG1 from '../assets/bg-2.svg'; 
+import BG2 from '../assets/bg-3.svg'; 
 const Login = (props) => {
   const { email, setEmail, password, setPassword, handleLogin, handleSignup, handleLogout, hasAccount, setHasAccount, emailError, passwordError } = props;
   return (
     <section className='login'>
+      <img className = "bg-asset left" src = {BG1} />
+      <img className = "bg-asset right" src = {BG2} />
       <div className='login-container'>
         <h4>Usuario</h4>
         <input type="text" autoFocus autoCapitalize required value={email} onChange={(
